75bb814e2d99338f9063e9a9cf048e949e78e97c
kavyyashre <kavyayarraguntla@gmail.com>
2026-02-28 09:53:14 +0530

    git push

---
+++ ListDemo.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+class ListDemo 
+{
+    public static void main(String[] args) 
+    {
+        List<String> fruits = new ArrayList<>();
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("PineApple");
+        fruits.add("Papaya");
+
+    System.out.println(fruits);
+    }
+}
