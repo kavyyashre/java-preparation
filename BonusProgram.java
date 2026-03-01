c50c982388d0f87f252281a925bc3763175b2d66
kavyyashre <kavyayarraguntla@gmail.com>
2026-03-01 15:39:40 +0530

    Gitpush

---
+++ BonusProgram.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class BonusProgram{
+    public static void main(String[] args) {
+        
+        double bonus=0;
+        Scanner sc=new Scanner (System.in);
+
+        System.out.print("enter your salary: ");
+        double salary =sc.nextDouble();
+
+        if(salary<=30000){
+            bonus=0;
+        }
+        else if (salary >30000 && salary<50000){
+            bonus=salary*0.05;
+        }
+        else{
+            bonus=salary*0.10;
+        }
+
+        double finalsalary=bonus+salary;
+        System.out.println("your bonus is: "+ bonus);
+        System.out.println("your finalsalary is: "+finalsalary);
+    }
+}
